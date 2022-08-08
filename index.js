d5f88333a672dca59b24b9c2e6ab76f8ab02ccb9
maryamhaitham <marym.ihnj@gmail.com>
2022-08-08 09:34:25 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat (Tina) {
+  console.log(cats.push(Tina));
+}
+
+function destructivelyPrependCat (Tina) {
+  console.log(cats.unshift(Tina));
+}
+
+function destructivelyRemoveLastCat () {
+  console.log(cats.pop());
+}
+
+function destructivelyRemoveFirstCat () {
+  console.log(cats.shift());
+}
+
+function appendCat (Tina) {
+  return [...cats, Tina];
+}
+
+function prependCat (Tina) {
+  return [Tina, ...cats];
+}
+
+function removeFirstCat () {
+  return cats.slice(1);
+}
+
+function removeLastCat () {
+  return cats.slice(0, cats.length - 1);
+}
